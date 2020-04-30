62041ae921ba23ac00f5120cc260d28b0c5b2952
Haoli Yang <hyang77@hawk.iit.edu>
2020-04-30 15:32:52 +0800

    Added dropdown list

--- src/Components/BreedSelect.js
+++ src/Components/BreedSelect.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function BreedSelect() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <label>Select Breed: </label>
+      <select id="breeds">
+        <option value="husky">Husky</option>
+        <option value="golden retriever">Golden Retriever</option>
+        <option value="shiba">Shiba</option>
+      </select>
+    </React.Fragment>
+  );
 }
 
 export default BreedSelect;
