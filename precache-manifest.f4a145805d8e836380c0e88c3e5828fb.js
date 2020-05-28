6fb970606877ae424ffad7779a4ff5b567dee03b
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-28 16:58:59 +0800

    Updates

---
+++ precache-manifest.f4a145805d8e836380c0e88c3e5828fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6649b22561cab246b8b44073cc9767",
+    "url": "/dog-gallery-react/index.html"
+  },
+  {
+    "revision": "e8e7e654d2dbcdb1e5c9",
+    "url": "/dog-gallery-react/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "58c3fc1a282993f5c50b",
+    "url": "/dog-gallery-react/static/css/main.c8cf117b.chunk.css"
+  },
+  {
+    "revision": "e8e7e654d2dbcdb1e5c9",
+    "url": "/dog-gallery-react/static/js/2.17e1927a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog-gallery-react/static/js/2.17e1927a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c3fc1a282993f5c50b",
+    "url": "/dog-gallery-react/static/js/main.4569bc25.chunk.js"
+  },
+  {
+    "revision": "9952fb7d1f857dbb14ab",
+    "url": "/dog-gallery-react/static/js/runtime-main.caffbbbf.js"
+  }
+]);
